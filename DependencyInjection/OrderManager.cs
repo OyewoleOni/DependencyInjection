5299dc4005ecf63a21aca1c7d82c4651144fe6cd
Oni Oyewole <ooni@eminenttechnology.com>
2019-04-29 13:43:17 +0100

    DI

--- DependencyInjection/OrderManager.cs
+++ DependencyInjection/OrderManager.cs
@@ -10,19 +10,30 @@ namespace DependencyInjection
     }
    public  class OrderManager : IOrderManager
     {
+        private readonly IProductStockRepository _productStockRepository;
+        private readonly IPaymentProcessor _paymentProcessor;
+        private readonly IShippingProcessor _shippingProcessor;
+
+        public OrderManager(IProductStockRepository productStockRepository, 
+            IPaymentProcessor paymentProcessor, IShippingProcessor shippingProcessor)
+        {
+            _productStockRepository = productStockRepository;
+            _paymentProcessor = paymentProcessor;
+            _shippingProcessor = shippingProcessor;
+        }
+
         public void Submit(Product product, string creditCardNumber, string expiryDate)
         {
             // Check product stock
-            var productStockRepository = new ProductStockRepository();
-            if ( !productStockRepository.IsInStock(product) )
+            
+            if ( !_productStockRepository.IsInStock(product) )
                 throw new Exception($"{product.ToString()} currently not in stock");
             // Payment
-            var paymentProcessor = new PaymentProcessor();
-            paymentProcessor.ChargeCreditCard(creditCardNumber, expiryDate);
+           
+            _paymentProcessor.ChargeCreditCard(creditCardNumber, expiryDate);
 
             // Ship the product
-            var shippingProcessor = new ShippingProcessor();
-            shippingProcessor.MailProduct(product);
+            _shippingProcessor.MailProduct(product);
         }
     }
 }
--- DependencyInjection/ShippingProcessor.cs
+++ DependencyInjection/ShippingProcessor.cs
@@ -6,10 +6,15 @@ namespace DependencyInjection
 {
    public class ShippingProcessor : IShippingProcessor
     {
+        private readonly IProductStockRepository _productStockRepository;
+
+        public ShippingProcessor(IProductStockRepository productStockRepository)
+        {
+            _productStockRepository = productStockRepository;
+        }
         public void MailProduct(Product product)
         {
-            var productStockRepository = new ProductStockRepository();
-            productStockRepository.ReduceStock(product);
+            _productStockRepository.ReduceStock(product);
 
             Console.WriteLine("Call to Shipping API");
         }
